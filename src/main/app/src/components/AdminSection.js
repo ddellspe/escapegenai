3cb66d9037f0960d6c9c299ad88daf62bf7318a6
David Dellsperger <david.dellsperger@gmail.com>
2024-06-24 21:00:49 -0500

    Add initial support for Admin Section in the UI

---
+++ src/main/app/src/components/AdminSection.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from 'react';
+import Alert from '@mui/material/Alert';
+import Box from '@mui/material/Box';
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
+import Fab from '@mui/material/Fab';
+import LoginIcon from '@mui/icons-material/LoginTwoTone';
+import LogoutIcon from '@mui/icons-material/LogoutTwoTone';
+import Snackbar from '@mui/material/Snackbar';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import LoginForm from "./LoginForm";
+
+export default function AdminSection() {
+  var creds = sessionStorage.getItem('auth');
+  const [auth, setAuth] = useState(false);
+  const [openSpeedDialOptions, setOpenSpeedDialOptions] = useState(false);
+  const [openLoginModal, setOpenLoginModal] = useState(false);
+  const [loginError, setLoginError] = useState(false);
+  const [dataSent, setDataSent] = useState("");
+  const toggleDial = () => setOpenSpeedDialOptions((open) => !open);
+  const killAlert = () => {
+    setDataSent("");
+  }
+  const handleCloseAll = (success, message) => {
+    if (typeof success === "boolean") {
+      if (success) {
+        setDataSent(message);
+      }
+    }
+    setOpenSpeedDialOptions(false);
+    setOpenLoginModal(false);
+  };
+  const handleOpenLoginModal = () => {
+    setOpenSpeedDialOptions(false);
+    setOpenLoginModal(true);
+  };
+  const logout = () => {
+    sessionStorage.removeItem('auth');
+    setAuth(false);
+  };
+  const handleLogin = () => {
+    creds = sessionStorage.getItem('auth');
+    handleCloseAll();
+  }
+
+  useEffect(() => {
+    if (creds !== null) {
+      fetch('session', {headers: new Headers({'Authorization': 'Basic ' + creds})})
+      .then((response) => {
+        if(!response.ok) {
+          setAuth(false);
+          setLoginError(true);
+          setTimeout(() => setLoginError(false), 5000);
+          sessionStorage.removeItem('auth');
+        } else {
+          setAuth(true);
+        }
+      });
+    }
+  }, [creds]);
+
+  if (auth === true) {
+    return (
+        <Box>
+          <SpeedDial
+              ariaLabel="controlled open manageData"
+              sx={{ position: 'absolute', bottom: 16, right: 16 }}
+              icon={<SpeedDialIcon icon={<MenuIcon />} openIcon={<CloseIcon />} />}
+              onClick={toggleDial}
+              open={openSpeedDialOptions}
+              hidden={!auth}
+              FabProps={{ color:"info" }}
+          >
+            <SpeedDialAction
+                key="logout"
+                icon={<LogoutIcon />}
+                tooltipTitle="Logout"
+                onClick={logout} />
+          </SpeedDial>
+          <Snackbar
+              anchorOrigin={{vertical:'top', horizontal: 'center'}}
+              open={dataSent !== ""}
+              autoHideDuration={6000}
+              onClose={killAlert}
+          >
+            <Alert severity="success" sx={{width: '100%'}}>{dataSent}</Alert>
+          </Snackbar>
+        </Box>
+    );
+  } else {
+    return (
+        <Box>
+          <Fab
+              sx={{ position: 'absolute', bottom: 16, right: 16 }}
+              onClick={handleOpenLoginModal}
+              hidden={auth}
+          >
+            <LoginIcon />
+          </Fab>
+          <LoginForm opened={openLoginModal} onClose={handleLogin} />
+          <Snackbar
+              anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+              open={loginError}
+              message="Error on login"
+              key="loginError" />
+        </Box>
+    );
+  }
+};
